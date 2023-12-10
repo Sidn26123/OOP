8ea496ba961ac52b7797ccaa1ad271b72d7a1841
unknown <nguyenthanht632@gmail.com>
2023-12-10 11:23:33 +0700

    Thêm ID_User vào Thu, tạo UI DS Thu, tạo UI calendar

--- QLTHUCHI/src/main/Main.java
+++ QLTHUCHI/src/main/Main.java
@@ -1,18 +1,5 @@
-
-
 package main;
 import LoginSignup.View.Login;
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author LENOVO
- */
 public class Main {
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
